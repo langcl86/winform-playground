8b103d1f997a3329c5bb6433db760cd5bbc9d946
langcl86 <96951049+langcl86@users.noreply.github.com>
2022-12-15 18:05:36 -0500

    Update winforms.ps1

--- winforms.ps1
+++ winforms.ps1
@@ -1,56 +1,30 @@
 ﻿Add-Type -AssemblyName System.Windows.FOrms;
 
-$colCt = 10;
+## Table Size
+$colCt = 5;
 $rowCt = 5;
-$marX = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width - ([System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width - 80);
-$marY = 20;
 
+## Cell Size
+$cellWidth = 100;
+$cellHeight = 100;
+
+## Table Margin
+$marX = 10;
+$marY = 10;
 
 function addCell () {
     $panel = New-Object System.Windows.Forms.Panel;
-    $panel.BorderStyle = [System.Windows.Forms.BorderStyle]::Fixed3D;
-    $panel.Width = "100";
-    $panel.Height = "100";
+    $panel.BorderStyle = [
+    System.Windows.Forms.BorderStyle]::FixedSingle;
+    $panel.Width = $cellWidth;
+    $panel.Height = $cellHeight;
     return $panel;
-}
-
-function Panel2 {
-
-    if ($main.Controls.Contains($panel2)) {
-         $main.Controls.Remove($panel2);
-    }
-    else { 
-        [double]$p2x = [System.Math]::Round($main.Width / 4);
-        [double]$p2y = [System.Math]::Round($main.Height / 4);
-        
-        [double]$p2w = [System.Math]::Round($main.Width / 2);
-        [double]$p2h = [System.Math]::Round($main.Height / 1.75);
-
-        $panel2.Width = $p2w;
-        $panel2.Height = $p2h;
-
-        $panel2.Location = "$p2x, $p2y";
-        $main.Controls.Add($panel2);
-        $panel2.BringToFront();
-    }
 }
 
 $main = New-Object System.Windows.Forms.Form;
 $main.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog;
-$main.Width = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width - 80;
-$main.Height = [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Height - 200;
-
-$panel2 = [System.Windows.Forms.Panel]::new();
-$panel2.Name = "Panel 2";
-$panel2.BackColor = [System.Drawing.Color]::Chartreuse;
-$panel2.BorderStyle = [System.Windows.Forms.BorderStyle]::FixedSingle;
-
-$close_panel2 = [System.Windows.Forms.Button]::new();
-$close_panel2.Text = " X ";
-$close_panel2.Width = 30;
-$close_panel2.Add_Click({Panel2;});
-
-$panel2.Controls.Add($close_panel2);
+$main.Width = $cellWidth * $colCt + 50;;
+$main.Height = $cellHeight * $rowCt + 50;
 
 $cellCt = $rowCt * $colCt;
 for ($i = 0; $i -lt $cellCt; $i++) {
@@ -58,17 +32,16 @@ for ($i = 0; $i -lt $cellCt; $i++) {
     $cols = $colCt;
 
     [double]$row = [System.Math]::Truncate(([decimal]$i / $cols));
-    [double]$col =  $i % $cols;
+    [double]$col =  $i % $cols
 
-    if ($col -gt 0) { $x = $col * 100 + $marX; }     ## Not First Column
-    else { $x = $marX; }  ## First Column
+    if ($col -gt 0) { $x = $col * $cellWidth + $marX; }
+    else { $x = $marX; }
 
-    if ($row -gt 0) { $y = ($row * 100 + $marY); }   ## Not First Row 
-    else { $y = $marY; }  ## First Row
+    if ($row -gt 0) { $y = ($row * $cellHeight + $mary); }
+    else { $y = $marY; }
     
     $cell = "`$cell$i";
     Invoke-Expression -Command "$cell = addCell;";
-    Invoke-Expression -Command "$cell.Name = `"Cell $i`";";
     Invoke-Expression -Command "$cell.Location = `"$x, $y`";"
     Invoke-Expression -Command "$cell.Add_MouseLeave({
         switch ($cell.BackColor)
@@ -77,9 +50,6 @@ for ($i = 0; $i -lt $cellCt; $i++) {
             ([System.Drawing.Color]::Black)    { $cell.BackColor = [System.Drawing.SystemColors]::Control; }
         }
     });";
-    Invoke-Expression -Command "$cell.Add_Click({
-        Panel2;
-    });";
     Invoke-Expression -Command "$cell.Add_MouseHover({
         switch ($cell.BackColor)
         {
@@ -87,7 +57,6 @@ for ($i = 0; $i -lt $cellCt; $i++) {
             ([System.Drawing.Color]::Black)    { $cell.BackColor = [System.Drawing.SystemColors]::Control; }
         }
     });";
-
     Invoke-Expression -Command "`$main.Controls.Add($cell);";
 }
 
